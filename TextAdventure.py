d620fe4892eaae2003bef168fd9bdf79fdd07dbf
TheAtomicJoker <tetteyd@coventry.ac.uk>
2015-10-09 12:52:50 +0100

    Update TextAdventure.py

--- TextAdventure.py
+++ TextAdventure.py
@@ -1,19 +1,31 @@
 print("----------121COM Week 2 Lab Exercise 7----------")
-print("----------------<InsertTitleHere>---------------")
-print("---------------<InsertAuthorsHere>--------------")
+print("----------------<District22>---------------")
+print("---------------<TheAtomKIRA>--------------")
 print("\n")
 
-print("You awake in a dark room.  Do you:")
+print("You wake up hanging off a mile high building.  Do you:")
 print("a) Scream for help.")
-print("b) Press the light switch")
-x = input("Enter a or b: ")
-if x == "a":
-    print("Someone hears your screams...")
+print("b) try and acsend the rope your hanging from")
+print("c) break a window and use the glass to cut you loose and fall to your death
+x = input("Enter a or b or c: ")
+if x == "b":
+    print("a mysterious figue appears at the beggining of the rope...")
+    # Contine adventure Here
+    print("its a woman....  do you:")
+    print("a) call for her to pull you up)  
+    print("b) climb the rope even faster)
+    print("c)give up as you assume to know her)
+elif x == "a":
+    print("the woman burst out in laughter...she then begins to cut the rope from the base.    do you?)
+    print("a) give up and let her cut the rope making you fall to your death)
+    print("b)shout the name Diana out loud??)
     # Contine adventure Here
 elif x == "b":
-    print("The light comes on.")
-    print("You do not recognise the room but you have a really bad feeling...")
-    # Contine adventure Here
+    print ("The woman comes to a complete stop and shouts how do u know my name?.   do you?)
+    print("a)say u dont know 
+    print("b)say its written on your shirt
+elif x == "b":
+    print("throug rage of your stupid comment she taakes a sweing with her blade at the rop and cuts you loose
 else:
     print("That was not an option.  Game Over")
 
